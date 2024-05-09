2b34c810de2b5c539c9ff16ebe50336f72219411
sifat26 <sifatict26@gmail.com>
2024-05-10 01:06:51 +0600

    Login and Register page added

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,8 +1,10 @@
+import { Navbar } from "@material-tailwind/react";
 import { Outlet } from "react-router-dom";
 
 const Root = () => {
     return (
         <div>
+            <Navbar/>
             <Outlet></Outlet>
             
         </div>
--- src/Pages/Shared/NavBar.jsx
+++ src/Pages/Shared/NavBar.jsx
@@ -8,33 +8,10 @@ import {
   Tooltip,
   Avatar,
 } from "@material-tailwind/react";
-import { NavLink } from "react-router-dom";
-import { AuthContext } from "./Authentication/AuthProvider";
 
 
-export function NavigateBar() {
-  const [theme, setTheme] = useState(
-    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
-  );
+export function NavBar() {
 
-  // update state on toggle
-  const handleToggle = (e) => {
-    if (e.target.checked) {
-      setTheme("dark");
-    } else {
-      setTheme("light");
-    }
-  };
-
-  // set theme state in localstorage on mount & also update localstorage on state change
-  useEffect(() => {
-    localStorage.setItem("theme", theme);
-    const localTheme = localStorage.getItem("theme");
-    // add custom data-theme attribute to html tag required to update theme using DaisyUI
-    document.querySelector("html").setAttribute("data-theme", localTheme);
-  }, [theme]);
-  const { user, logOut } = useContext(AuthContext);
-  // console.log("sei",user);
   const handleLogout = () => {
     logOut()
       .then(() => console.log("User Logged Out Succesfully"))
