dccf14bcb945478181d6b6f6f6ca419ec78dfe82
sifat26 <sifatict26@gmail.com>
2024-05-11 11:31:33 +0600

    Footer Added

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { NavBar } from "../Pages/Shared/NavBar";
+import Footer from "../Pages/Shared/Footer";
 
 const Root = () => {
     return (
         <div>
             <NavBar/>
             <Outlet></Outlet>
+            <Footer/>
             
         </div>
     );
